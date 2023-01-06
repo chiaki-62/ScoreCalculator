62c15224d5b088be4cc87c33121f893751c5b83d
unknown <zero.oneok6@gmail.com>
2023-01-07 01:11:56 +0900

    入力してラベルに追加は可能

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _text = '';
+  // テキストフィールドを管理するコントローラを作成(入力内容の取得用)
+  final textController = TextEditingController();
 
-  _addName(text) {
-    debugPrint(text);
+  // 入力データ格納用のリスト
+  List<Map<String, dynamic>> items = [];
+
+  //　ID（カウンタ変数）
+  int _counter = 0;
+
+  //　追加ボタンが押されたときの処理（リストにIDと入力データを新規追加）
+  void _addItem(String inputText) {
+    setState(() {
+      _counter++;
+      items.add({ "id": _counter, "content": inputText});
+    });
+  }
+
+  @override
+  // widgetの破棄時にコントローラも破棄
+  void dispose() {
+    textController.dispose();
+    super.dispose();
   }
 
   @override
@@ -40,60 +58,53 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(top:8,left:8,right:8),
-            child: Column(
-              children: const <Widget>[
-                Card(
-                  child: ListTile(
-                    title: Text('_text'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('タマネギを買う'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('ジャガイモを買う'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('カレールーを買う'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(top:16,left:16,right:16),
-            child : TextFormField(
-              onChanged: (value) {
-                _text = value;
-              },
-              decoration: const InputDecoration(
-                labelText: '名前',
-                border: OutlineInputBorder(),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child:ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = items[index];
+                  // 新しいカードを作成して返す
+                  return Card(
+                    child: ListTile(
+                      title: Text(item["id"].toString() + " : " + item["content"]),
+                    ),
+                  );
+                }
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16),
-            child : ElevatedButton(
-              onPressed: () {
-                _addName(_text);
-              },
-              style: ElevatedButton.styleFrom(
-                elevation: 16,
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextFormField(
+                controller: textController,
+                decoration: const InputDecoration(
+                  labelText: '名前',
+                  border: OutlineInputBorder(),
+                ),
               ),
-              child: const Text('メンバーを追加する'),
             ),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.all(16),
+              child : ElevatedButton(
+                onPressed: () {
+                  //テキストフィールドの内容を取得し、アイテムリストに追加
+                  _addItem(textController.text);
+
+                  // テキストフィールドの内容をクリア
+                  textController.clear();
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 16,
+                ),
+                child: const Text('メンバーを追加する'),
+              ),
+            ),
+          ]
+        ),
       ),
     );
   }
